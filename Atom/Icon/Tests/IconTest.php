fcfa746092b450d8453c4c30d807b9a4d2a89b4e
dpi <pro@danielph.in>
2025-07-25 09:21:28 +0800

    ⬆️✨ Drupal core 11.2 + Gin + PHPStan v2 (#45)

--- Atom/Icon/Tests/IconTest.php
+++ Atom/Icon/Tests/IconTest.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace PreviousNext\Ds\Common\Component\Card\Tests;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
+use PHPUnit\Framework\Attributes\CoversMethod;
 use PHPUnit\Framework\TestCase;
 use PreviousNext\Ds\Common\Atom as CommonAtoms;
 use PreviousNext\Ds\Common\List\CommonLists;
 use PreviousNext\Ds\Mixtape\Atom;
+use PreviousNext\Ds\Mixtape\Atom\Icon\Icon;
 use PreviousNext\Ds\Mixtape\List\MixtapeLists;
 use PreviousNext\Ds\Nsw\Lists\NswLists;
 use PreviousNext\IdsTools\DependencyInjection\IdsContainer;
 
+#[CoversMethod(Icon::class, 'createMixtapeIcon')]
 class IconTest extends TestCase {
 
   protected function setUp(): void {
@@ -29,16 +32,13 @@ class IconTest extends TestCase {
     $container->compile();
   }
 
-  /**
-   * @covers \PreviousNext\Ds\Mixtape\Atom\Icon\Icon::createMixtapeIcon
-   */
   public function testCreateMixtapeIcon(): void {
     $icon = CommonAtoms\Icon\Icon::create();
 
     // Ensure we got Mixtapes icon class as we are testing Mixtapes build.
-    static::assertInstanceOf(Atom\Icon\Icon::class, $icon);
+    static::assertInstanceOf(Icon::class, $icon);
 
-    $icon = Atom\Icon\Icon::createMixtapeIcon(Atom\Icon\Icons::Drupal, Atom\Icon\IconSize::ExtraLarge);
+    $icon = Icon::createMixtapeIcon(Atom\Icon\Icons::Drupal, Atom\Icon\IconSize::ExtraLarge);
     $build = $icon();
     static::assertEquals('drupal', $build['#icon']);
     // Ensure the icon didn't end up as another class in the attributes object, and only the size did:
