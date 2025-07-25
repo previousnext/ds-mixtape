6acee385b2dee71f259b25c9381747fe6c0ae4fb
dpi <pro@danielph.in>
2025-07-25 09:44:48 +0800

    ✨ Allow design systems to be installed simultaneously (#46)

--- Atom/Icon/Tests/IconTest.php
+++ Atom/Icon/Tests/IconTest.php
@@ -4,15 +4,11 @@ declare(strict_types=1);
 
 namespace PreviousNext\Ds\Common\Component\Card\Tests;
 
-use Drupal\Core\DependencyInjection\ContainerBuilder;
 use PHPUnit\Framework\Attributes\CoversMethod;
 use PHPUnit\Framework\TestCase;
 use PreviousNext\Ds\Common\Atom as CommonAtoms;
-use PreviousNext\Ds\Common\List\CommonLists;
 use PreviousNext\Ds\Mixtape\Atom;
 use PreviousNext\Ds\Mixtape\Atom\Icon\Icon;
-use PreviousNext\Ds\Mixtape\List\MixtapeLists;
-use PreviousNext\Ds\Nsw\Lists\NswLists;
 use PreviousNext\IdsTools\DependencyInjection\IdsContainer;
 
 #[CoversMethod(Icon::class, 'createMixtapeIcon')]
@@ -20,16 +16,8 @@ class IconTest extends TestCase {
 
   protected function setUp(): void {
     parent::setUp();
-    $container = new ContainerBuilder();
-    $container->setParameter('ids.design_system', 'mixtape');
-    $container->setParameter('ids.design_system.additional', ['common']);
-    $container->setParameter('ids.design_systems', [
-      'mixtape' => MixtapeLists::Lists,
-      'nsw' => NswLists::Lists,
-      'common' => CommonLists::Lists,
-    ]);
-    IdsContainer::setupContainer($container);
-    $container->compile();
+
+    IdsContainer::testContainerForDs('mixtape');
   }
 
   public function testCreateMixtapeIcon(): void {
