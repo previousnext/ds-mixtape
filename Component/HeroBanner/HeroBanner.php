8f7507ecea300c81de0c5e8a0a40d17590265598
dpi <pro@danielph.in>
2025-08-21 15:35:42 +0800

    ✨ Hero banner should support a button in its link slot (#73)
    - Highlight docs
    - Don't default highlight to true
    - Subtitle should be optional
    - Add default background for hero banner.

--- Component/HeroBanner/HeroBanner.php
+++ Component/HeroBanner/HeroBanner.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace PreviousNext\Ds\Mixtape\Component\HeroBanner;
 
+use Drupal\Core\Template\Attribute;
 use Pinto\Attribute\Asset\Css;
 use Pinto\Slots;
 use PreviousNext\Ds\Common\Component as CommonComponent;
 use PreviousNext\Ds\Mixtape\Utility;
 use PreviousNext\IdsTools\Scenario\Scenarios;
 
-/**
- * @see https://mixtape.pnx.io/section-grouped-components.html#kssref-grouped-components-page-title-hero
- */
 #[Css('hero-banner.css', preprocess: TRUE)]
 #[Slots\Attribute\RenameSlot(original: 'links', new: 'linkList')]
 #[Scenarios([CommonComponent\HeroBanner\HeroBannerScenarios::class])]
@@ -21,15 +19,17 @@ class HeroBanner extends CommonComponent\HeroBanner\HeroBanner implements Utilit
 
   protected function build(Slots\Build $build): Slots\Build {
     return parent::build($build)
+      ->set('containerAttributes', new Attribute())
       // Mixtape `modifiers` may only contain values from
       // HeroBannerBackground::modifierName().
       ->set('modifiers', $this->modifiers->getInstancesOf(HeroBannerBackground::class)->map(
-        static fn (HeroBannerBackground $modifier): string => $modifier->modifierName(),
+        static fn (HeroBannerBackground $modifier): ?string => $modifier->modifierName(),
       ))
       ->set('title', $this->title)
       ->set('subtitle', $this->subtitle)
       ->set('link', $this->link)
       ->set('image', $this->image)
+      ->set('highlight', $this->highlight)
       ->set('links', $this->links);
   }
 
--- Component/HeroBanner/HeroBannerBackground.php
+++ Component/HeroBanner/HeroBannerBackground.php
@@ -9,12 +9,15 @@ use PreviousNext\Ds\Common\Modifier\Mutex;
 
 #[Mutex]
 enum HeroBannerBackground implements HeroBannerModifierInterface {
+
+  case White;
   case Dark;
   case OffWhite;
   case Light;
 
-  public function modifierName(): string {
+  public function modifierName(): ?string {
     return match ($this) {
+      static::White => NULL,
       static::Dark => 'dark',
       static::OffWhite => 'off-white',
       static::Light => 'light',
