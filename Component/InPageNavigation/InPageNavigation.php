e67967b0d00ae3936fd3e367f9fd5130bf142f87
dpi <pro@danielph.in>
2026-01-15 10:12:57 +0800

    fix(inpagenavigation): Fix heading slot names for InPageNavigation in Mixtape (#170)

--- Component/InPageNavigation/InPageNavigation.php
+++ Component/InPageNavigation/InPageNavigation.php
@@ -10,11 +10,15 @@ use PreviousNext\Ds\Common\Component as CommonComponents;
 use PreviousNext\Ds\Mixtape\Utility;
 use PreviousNext\IdsTools\Scenario\Scenarios;
 
+/**
+ * Searches areas with '.js-content' selector (see Twig).
+ */
 #[Asset\Css('in-page-navigation.css', preprocess: TRUE)]
 #[Asset\Js('in-page-navigation.entry.js', preprocess: TRUE, attributes: ['type' => 'module'])]
 #[Scenarios([CommonComponents\InPageNavigation\InPageNavigationScenarios::class])]
 #[Slots\Attribute\RenameSlot(original: 'containerAttributes', new: 'attributes')]
 #[Slots\Attribute\RenameSlot(original: 'includeElements', new: 'levels')]
+#[Slots\Attribute\RenameSlot(original: 'heading', new: 'title')]
 class InPageNavigation extends CommonComponents\InPageNavigation\InPageNavigation implements Utility\MixtapeObjectInterface {
 
   use Utility\ObjectTrait;
