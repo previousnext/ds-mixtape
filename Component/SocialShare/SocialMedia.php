488f259fdca95e66aaed320c5ce540c8e65b6058
dpi <pro@danielph.in>
2026-01-19 19:50:19 +0800

    PNXI-444: Fix current URL generation for Mixtape Social Share (#179)

--- Component/SocialShare/SocialMedia.php
+++ Component/SocialShare/SocialMedia.php
@@ -17,11 +17,11 @@ enum SocialMedia implements SocialShareSocialMediaInterface {
 
   public function defaultUrl(): Url {
     return match ($this) {
-      static::Facebook => Url::fromUri(\sprintf('https://www.facebook.com/sharer/sharer.php?u=%s', Url::fromUri('<current>')->toString())),
-      static::LinkedIn => Url::fromUri(\sprintf('https://www.linkedin.com/sharing/share-offsite/?url=%s', Url::fromUri('<current>')->toString())),
-      static::Twitter => Url::fromUri(\sprintf('https://twitter.com/intent/tweet?url=%s', Url::fromUri('<current>')->toString())),
-      static::Bluesky => Url::fromUri(\sprintf('https://bsky.app/intent/compose?text=%s', Url::fromUri('<current>')->toString())),
-      static::Email => Url::fromUri(\sprintf('mailto:?subject=&body=%s', Url::fromUri('<current>')->toString())),
+      static::Facebook => Url::fromUri(\sprintf('https://www.facebook.com/sharer/sharer.php?u=%s', Url::fromRoute('<current>')->setAbsolute()->toString())),
+      static::LinkedIn => Url::fromUri(\sprintf('https://www.linkedin.com/sharing/share-offsite/?url=%s', Url::fromRoute('<current>')->setAbsolute()->toString())),
+      static::Twitter => Url::fromUri(\sprintf('https://twitter.com/intent/tweet?url=%s', Url::fromRoute('<current>')->setAbsolute()->toString())),
+      static::Bluesky => Url::fromUri(\sprintf('https://bsky.app/intent/compose?text=%s', Url::fromRoute('<current>')->setAbsolute()->toString())),
+      static::Email => Url::fromUri(\sprintf('mailto:?subject=&body=%s', Url::fromRoute('<current>')->setAbsolute()->toString())),
     };
   }
 
