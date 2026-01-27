f836ba5d452acd0507349b295f90279a32a369d5
dpi <pro@danielph.in>
2026-01-27 15:00:44 +0800

    feat(accordion): `Accordion` component should accept anything and autowrap with `AccordionItem` (#195)

--- Component/Accordion/AccordionItem/AccordionItem.php
+++ Component/Accordion/AccordionItem/AccordionItem.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace PreviousNext\Ds\Mixtape\Component\Accordion\AccordionItem;
 
+use Drupal\Core\Render\Markup;
+use Pinto\Slots;
+use PreviousNext\Ds\Common\Atom\Html\Html;
 use PreviousNext\Ds\Common\Component as CommonComponent;
 use PreviousNext\Ds\Mixtape\Utility;
 
 class AccordionItem extends CommonComponent\Accordion\AccordionItem\AccordionItem implements Utility\MixtapeObjectInterface {
 
   use Utility\ObjectTrait;
+
+  protected function build(Slots\Build $build): Slots\Build {
+    $build = parent::build($build);
+
+    if ($this->title === NULL || $this->title === '') {
+      // Mixtape title must have something substantial otherwise the button will shift to the left.
+      $build->set('title', Html::create(Markup::create('&nbsp;')));
+    }
+
+    return $build;
+  }
 
 }
